66a46ab024c4d5b52a086463f0bcda3914ba620e
slowdata <slowmotion@outlook.com>
2019-02-04 02:11:32 +0000

    Follow the book till Unit Tests

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
 import Search from "./components/Search";
 import Table from "./components/Table";
 import Button from "./components/Button";
 
-// API parameters
-const DEFAULT_QUERY = "redux";
-const DEFAULT_HPP = "100";
-
-const PATH_BASE = "https://hn.algolia.com/api/v1";
-const PATH_SEARCH = "/search";
-const PARAM_SEARCH = "query=";
-const PARAM_PAGE = "page=";
-const PARAM_HPP = "hitsPerPage=";
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP
+} from "./config";
 
 // const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}0&${PARAM_HPP}${DEFAULT_HPP}`;
 
@@ -37,6 +38,8 @@ const PARAM_HPP = "hitsPerPage=";
 // ];
 
 class App extends Component {
+  _isMounted = false;
+
   state = {
     results: null,
     searchKey: "",
@@ -67,18 +70,22 @@ class App extends Component {
   };
 
   componentDidMount() {
+    this._isMounted = true;
     const { searchTerm } = this.state;
     this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
   }
 
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
   fetchSearchTopStories = (searchTerm, page = 0) => {
-    fetch(
+    axios(
       `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}&${PARAM_HPP}${DEFAULT_HPP}`
     )
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => this.setState({ error }));
+      .then(result => this._isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this._isMounted && this.setState({ error }));
   };
 
   onDismiss = ID => {
@@ -112,17 +119,13 @@ class App extends Component {
   };
 
   render() {
-    const { results, searchTerm, searchKey } = this.state;
+    const { results, searchTerm, searchKey, error } = this.state;
 
     const page =
       (results && results[searchKey] && results[searchKey].page) || 0;
 
     const list =
       (results && results[searchKey] && results[searchKey].hits) || [];
-
-    if (error) {
-      return <p>Somthing went wrong</p>;
-    }
 
     return (
       <div className="page">
@@ -135,7 +138,13 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table result={list} onDismiss={this.onDismiss} />
+        {error ? (
+          <div className="interactions">
+            <p>Something went wrong</p>
+          </div>
+        ) : (
+          <Table result={list} onDismiss={this.onDismiss} />
+        )}
         <div className="interactions">
           <Button
             onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import App from "./App";
+import Search from "./components/Search";
+import Button from "./components/Button";
+import Table from "./components/Table";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("has a valid snapshot", () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Search", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Search>Search</Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("has a valid snapshot", () => {
+    const component = renderer.create(<Search>Search</Search>);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Button", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Button>Give Me More</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("has a valid snapshot", () => {
+    const component = renderer.create(<Button>Give Me More</Button>);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Tabble", () => {
+  const props = {
+    list: [
+      { title: "1", author: "1", num_comments: 1, points: 2, objectID: "y" },
+      { title: "2", author: "2", num_comments: 1, points: 2, objectID: "z" }
+    ]
+  };
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Table {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("has a valid snapshot", () => {
+    const component = renderer.create(<Table {...props} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
